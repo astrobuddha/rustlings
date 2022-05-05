aa3703b8e5d260ea117046fe0b3f6bfc3b90a6a5
jozefdickins <jozefleo@protonmail.com>
2022-05-04 21:17:39 -0400

    finished structs and enums. Destructuring enums is important and enums are quit robust

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -1,8 +1,6 @@
 // structs2.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -16,13 +14,13 @@ struct Order {
 
 fn create_order_template() -> Order {
     Order {
-        name: String::from("Bob"),
-        year: 2019,
+        name: String::from("Hacker in Rust"),
+        year: 2022,
         made_by_phone: false,
         made_by_mobile: false,
         made_by_email: true,
-        item_number: 123,
-        count: 0,
+        item_number: 117,
+        count: 1,
     }
 }
 
@@ -34,7 +32,16 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            year: 2022,
+            made_by_phone: false,
+            made_by_mobile: false,
+            made_by_email: true,
+            item_number: 117,
+            count: 1
+        };
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
