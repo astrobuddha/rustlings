f99446b8ad1ea39521c72175aad67adeabb5aefb
jozefdickins <jozefleo@protonmail.com>
2022-05-16 20:53:49 -0400

    still not finished with iters4. but checking in progress

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -12,6 +12,8 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    [1..=num].fold(1, |fac, x| fac * x)
 }
 
 #[cfg(test)]
