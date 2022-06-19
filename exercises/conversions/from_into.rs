b308d2ccfaa68ce568da3c9f2213d3bd773ab6b2
jozefdickins <jozefleo@protonmail.com>
2022-06-19 14:03:44 -0400

    finish from intro but should double check

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+
+        if s.len() == 0 {
+            return Person::default()
+        }
+
+        let input: Vec<&str> = s.split(',').collect();
+        let name = input[0];
+        if input.len() != 2 || name.is_empty() {
+            Person::default()
+        } else {
+            match input[1].parse::<usize>() {
+                Ok(age) => Person {
+                    name: name.to_string(),
+                    age: age,
+                },
+                Err(_e) => Person::default()
+            }
+        }
     }
 }
 
